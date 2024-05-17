ed932333cfb5f949c328e151537632f95bb0984e
Christian Bustos <christianbus37@gmail.com>
2024-05-17 16:06:34 -0300

    proyecto terminado

---
+++ js/index_dom.js
@@ -0,0 +1,7 @@
+import countdown from "./countdown.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    countdown("countdown","November 19,2024","Feliz Cumple my friend 🍻");
+});
